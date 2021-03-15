8037d10116b7b365a6c83637b17e4151767ad2a2
Gordon Sim <gsim@redhat.com>
2021-03-15 21:51:56 +0000

    experiment with 4k buffer

--- src/buffer.c
+++ src/buffer.c
@@ -24,7 +24,7 @@
 #include <string.h>
 
 
-size_t BUFFER_SIZE     = 512;
+size_t BUFFER_SIZE     = 4096;
 
 ALLOC_DECLARE(qd_buffer_t);
 ALLOC_DEFINE_CONFIG(qd_buffer_t, sizeof(qd_buffer_t), &BUFFER_SIZE, 0);
